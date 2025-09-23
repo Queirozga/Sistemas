c543a1ff51c8b5f97d6dafa8223fcf2d3282b897
Queirozga <bielhenri1302@gmail.com>
2025-09-22 21:01:53 -0300

    do exercise of ArrayList

--- Java_Segundo_Sem/POO/PooAula4/OrdemServiço/src/ordemserviço/Servico.java
+++ Java_Segundo_Sem/POO/PooAula4/OrdemServiço/src/ordemserviço/Servico.java
@@ -1,38 +1,64 @@
 package ordemserviço;
 import java.util.Scanner;
 import model.OrdemServico;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Servico {   
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("-----Consultor de Ordem de Serviço-----");
-        System.out.println("\nInforme o id: ");
-        int id = sc.nextInt();
-        System.out.println("\nNome do Cliente: ");
-        String nomeCliente = sc.next();
-        System.out.println("\nNome Profissional: ");
-        String nomeProfissional = sc.next();
-        System.out.println("\nValor da hora trabalhada: ");
-        double valorServicoHora = sc.nextDouble();
-        System.out.println("\nHoras trabalhadas: ");
-        double nroHorasTrabalhadas = sc.nextDouble();
-        System.out.println("\nValor por Km deslocado: ");
-        double valorKmDeslocado = sc.nextDouble();
-        System.out.println("\nKms deslocados: ");
-        double nroKmPercorrido = sc.nextDouble();
+        OrdemServico s = new OrdemServico();
+        List <OrdemServico> ListServicos = new ArrayList<>();
         
-        OrdemServico s1 = new OrdemServico();
-        s1.setId(id);
-        s1.setNomeCliente(nomeCliente);
-        s1.setNomeProfissional(nomeProfissional);
-        s1.setValorServicoHora(valorServicoHora);
-        s1.setNroHorasTrabalhadas(nroHorasTrabalhadas);
-        s1.setValorKmDeslocamento(valorKmDeslocado);
-        s1.setNroKmPercorrido(nroKmPercorrido);
+        int escolha = 1;
         
-        System.out.println("\n-----Ordem de serviço-----");
-        System.out.println(s1.toString());
+        do{
+            System.out.println("\n-----Consultor de Ordem de Serviço-----\n");
+            System.out.println("Escolha o que quer fazer\n1- Calcular Ordem de servico\n9-Parar Programa");
+            escolha = sc.nextInt();
+                
+            switch (escolha){
+                case 1:                     
+                    System.out.println("\nInforme o id: ");
+                    int id = sc.nextInt();
+                    System.out.println("\nNome do Cliente: ");
+                    String nomeCliente = sc.next();
+                    System.out.println("\nNome Profissional: ");
+                    String nomeProfissional = sc.next();
+                    System.out.println("\nValor da hora trabalhada: ");
+                    double valorServicoHora = sc.nextDouble();
+                    System.out.println("\nHoras trabalhadas: ");
+                    double nroHorasTrabalhadas = sc.nextDouble();
+                    System.out.println("\nValor por Km deslocado: ");
+                    double valorKmDeslocado = sc.nextDouble();
+                    System.out.println("\nKms deslocados: ");
+                    double nroKmPercorrido = sc.nextDouble();
+
+                    s.setId(id);
+                    s.setNomeCliente(nomeCliente);
+                    s.setNomeProfissional(nomeProfissional);
+                    s.setValorServicoHora(valorServicoHora);
+                    s.setNroHorasTrabalhadas(nroHorasTrabalhadas);
+                    s.setValorKmDeslocamento(valorKmDeslocado);
+                    s.setNroKmPercorrido(nroKmPercorrido);
+
+                    ListServicos.add(s);
+
+                    for(OrdemServico ItemServicos : ListServicos){
+                        System.out.println("\n-----Ordem de serviço-----");
+                        System.out.println(ItemServicos.toString());
+                    };
+                    break;
+                case 9: 
+                    break;
+                default: 
+                    System.out.println("Numero invalido!");
+                    break;  
+        }
+        } while (escolha != 9);
+        
+        System.out.println(ListServicos.toString());
     }
     
 }
